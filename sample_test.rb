cab846777fff638796e22f0019a2c60be29d030b
Ryo-exid <sakura4oneok@gmail.com>
2021-07-24 11:57:23 +0000

    Minitest（テスト自動化）

--- sample_test.rb
+++ sample_test.rb
@@ -1,8 +1,8 @@
-require 'minitest/autorun' #　ライブラリを読み込む
+require 'minitest/autorun' # ライブラリを読み込む
 
 class Sampletest < Minitest::Test # SampletestクラスがMinitest::Testクラスを継承 (class〜endまでがテストコードの本体)
-  def test_sample #メソッド名をtest_で始めるのが必須
+  def test_sample # メソッド名をtest_で始めるのが必須
     assert_equal 'RUBY', 'ruby'.upcase # 実行結果を確認するための検証メソッド
-   #assert_equal 期待する結果, テスト対象となる値や式
+   # assert_equal 期待する結果, テスト対象となる値や式
   end
 end
