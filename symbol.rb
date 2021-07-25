fcc24039078ba73a7247c4ffd6d3e0f7b266bfb3
Ryo-exid <sakura4oneok@gmail.com>
2021-07-25 17:45:09 +0000

    メソッドのキーワード引数とハッシュ

--- symbol.rb
+++ symbol.rb
@@ -29,3 +29,51 @@ p currencies[:us] # 値を取り出す時は同じ
 
 
 puts '②---------------------------'
+
+
+# ハッシュに格納する値は異なるデータ型が混在するケースがよくある
+person = {
+  name: 'Alice', #文字列
+  age: 20, # 数値
+  friends: ['Bob', 'Calol'], # 配列
+  phones: { home: '1234-0000', mobile: '5678-0000' } # ハッシュ
+}
+
+p person[:age]
+p person[:friends]
+p person[:phones][:mobile]
+
+
+puts '---------------------------'
+
+
+=begin
+
+メソッドのキーワード引数を使うと可読性が上がる
+
+def メソッド名(キーワード引数1: デフォルト値1, キーワード引数2: デフォルト値2 )
+  メソッドの実装
+end
+
+=end
+
+def buy_burger(menu, drink: true, potato: true)
+  # ハンバーガーを購入
+  if drink
+    # ドリンクを購入
+  end
+  if potato
+    # ポテトを購入
+  end
+end
+
+# チーズバーガーとドリンクとポテトを購入（キーワード引数を持つメソッドを呼び出す時は"シンボル: 値"）
+buy_burger('cheese', drink: true, potato: true) # drink: true, potato: trueを省略しても良い
+
+# フィッシュバーガーとドリンクを購入
+buy_burger('fish', potato: false) # trueがデフォルトなので省略
+
+
+# キーワード引数と一致するハッシュであれば、メソッドの引数として渡すことが出来る
+params = { drink: true, potato: false }
+buy_burger('fish', params)
