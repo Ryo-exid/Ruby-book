5e90799dc61387cfd7b3140d66bdd7c1b3a57aa4
Ryo-exid <sakura4oneok@gmail.com>
2021-07-26 10:23:28 +0000

    ハッシュを使った擬似キーワード引数

--- hash2.rb
+++ hash2.rb
@@ -25,3 +25,18 @@ puts '①---------------------------'
 h = { us: 'dollar', india: 'rupee' }
 # 変数hのキーと値を**で展開（**を付けない場合は構文エラーになる）
 p g = { japan: 'yen', **h } #=> {:japan=>"yen", :us=>"dollar", :india=>"rupee"}
+
+
+puts '②---------------------------'
+
+
+# 擬似キーワード引数（ハッシュを受け取ってキーワード引数のように見せるテクニック）
+
+# ハッシュを引数として受け取り、擬似キーワード引数を実現する
+def buy_burger(menu, options = {})
+  drink = options[:drink]
+  potato = options[:potato]
+  # 省略
+end
+p buy_burger('cheese', drink: true, potato: true)
+p buy_burger('fish', salad: true) # 擬似キーワード引数はどんな値を渡してもエラーにならない（無効なキーをエラーにするには明示的な実装が必要）
