7fb87d4d53d3e187c5ec765cd78ce271d0a57ff8
Ryo-exid <sakura4oneok@gmail.com>
2021-07-27 08:41:28 +0000

    よく使われるイディオム（2)/ ||=を使った自己代入

--- idiom2.rb
+++ idiom2.rb
@@ -1,1 +1,18 @@
 # ||= を使った自己代入
+
+limit ||= 10 # 意味：「変数limitがnilまたはfalseであれば、10を代入する（それ以外はlimitの値をそのまま使う）」
+# limit = limit || 10   と考えるとわかりやすい
+
+
+
+limit = nil
+limit ||= 10
+p limit #=> 10
+
+
+limit = 20
+limit ||= 10
+p limit #=> 20
+
+
+puts '---------------------------'
