742536c901188505c8fa72f81439722867c5257d
Ryo-exid <sakura4oneok@gmail.com>
2021-07-27 08:54:35 +0000

    よく使われるイディオム（3）/ git add .を使った真偽値の型変換

--- idiom2.rb
+++ idiom2.rb
@@ -16,3 +16,40 @@ p limit #=> 20
 
 
 puts '---------------------------'
+
+
+# !!を使った真偽値の型変換（確実にtrueまたはfalseを返すイディオム）
+
+=begin
+
+def user_exists?
+  # データベースなどからユーザを探す（なければnil）
+  user = find_user
+  if user
+    true # userが見つかったのでtrue
+  else
+    false # userが見つからなかったのでfalse
+  end
+end
+
+=end
+
+#上のコードをコンパクトにする
+def user_exists?
+  !!find_user
+end
+
+
+=begin
+
+!は否定の演算子。
+!Aと書いた場合、Aが真であればfalseを、そうでなければtrueを返す。
+つまりtrueかfalseに変換される。
+ここでもう一度!で反転させると、元のAni対応する真偽値がtrueまたはfalseとして得ることが出来る。
+
+=end
+
+p !!true #=> true
+p !!1 #=> true
+p !! false #=> false
+p !! nil #=> false
