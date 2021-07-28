4f32a6c0657ec923abf641ba55433ed29a2a4edb
Ryo-exid <sakura4oneok@gmail.com>
2021-07-28 16:45:59 +0000

    テストコードのリファクタリング

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,19 +3,33 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
+  def setup # Minitestではsetupメソッドを定義するとテストメソッドの実行前に毎回setupメソッドが呼び出される
+    @umeda = Gate.new(:umeda) # Gateオブジェクトをインスタンス変数に格納
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+
+  def test_umeda_to_juso
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
   end
 
   def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_juso_to_to_mikuni
+    ticket = Ticket.new(150)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
