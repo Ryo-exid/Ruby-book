14d5bce38b23d5b574561a86643271fce2220afa
Ryo-exid <sakura4oneok@gmail.com>
2021-07-31 08:03:43 +0000

    deep_freezeメソッド/自動化テスト/[Add]extendを使う理由

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,8 @@
 require './lib/deep_freezable'
 
 class Team
+  # deep_freezeメソッドが呼ばれているのはクラス構文の直下。クラス構文の直下ではクラスメソッドしか呼び出せない。
+  # よって、deep_freezeメソッドはクラスメソッド（特異メソッド）としてミックスインする必要がある。 ※ includeではなくextendを使っているのはそのため。
   extend DeepFreezable
 
   COUNTRIES = deep_freeze(['Japan', 'US', 'India'] )
