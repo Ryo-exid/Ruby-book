4dae52429dd60f3ef221768751af077f144702aa
Ryo-exid <sakura4oneok@gmail.com>
2021-08-10 12:12:23 +0000

    有効範囲を限定できるrefinements

---
+++ module/refinements.rb
@@ -0,0 +1,34 @@
+# refinementsを使うと独自の変更の有効範囲（スコープ）を限定することができる = 予期せぬバグやエラーに遭遇するリスクを低減することができる
+
+# 以下、例としてrefinementsを使ってStringクラスに文字列の中身をランダムに入れ替えるshuffleメソッドを追加
+
+module StringShuffle
+  # refinementsが目的なので、refineメソッドを使う
+  refine String do
+    def shuffle
+      chars.shuffle.join
+    end
+  end
+end
+
+class User
+  using StringShuffle # refinementsを有効にするためにusingメソッドを使用
+
+  def initialize(name)
+    @name = name
+  end
+
+  def shuffled_name
+    # Userクラスの内部であればStringクラスのshuffleメソッドが有効になる
+    @name.shuffle
+  end
+
+  #Userクラスを抜けるとrefinementsは無効になる
+end
+
+user = User.new('Alice')
+# Userクラス内ではshuffleメソッドが有効になっている
+p user.shuffled_name #=> "ceAli"
+
+# Userクラスを経由しない場合はshuffleメソッドは使えない
+# p 'Alice'.shuffle #=> NoMethodError
