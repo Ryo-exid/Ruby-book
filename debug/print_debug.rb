345f01507aa16e18fb1480c110d58da1ee13a533
Ryo-exid <sakura4oneok@gmail.com>
2021-08-13 06:01:25 +0000

    printデバッグ/条件分岐の実行を確認

--- debug/print_debug.rb
+++ debug/print_debug.rb
@@ -9,3 +9,20 @@ end
 
 
 # -----------------------------------------------------------------
+
+
+# printデバッグは変数やメソッドの値を出力するだけではなく、メソッドや条件分岐が意図したとおりに実行されているかどうかを確認する場合にも使える。
+
+def greeting(country)
+  # greetingメソッドが呼ばれたことを確認
+  puts 'greeting start'
+  return 'countryを入力してください' if country.nil?
+
+  if country == 'japan'
+    puts 'japan' # 真の分岐に入ったことを確認
+    'こんにちは'
+  else
+    puts 'other' # 偽の分岐に入ったことを確認
+    'hello'
+  end
+end
