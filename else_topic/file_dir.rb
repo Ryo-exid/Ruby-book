5f871a42ec2d1fa2dae420ad884180a643c5f165
Ryo-exid <sakura4oneok@gmail.com>
2021-08-14 07:25:58 +0000

    txtの作成/読み書き

--- else_topic/file_dir.rb
+++ else_topic/file_dir.rb
@@ -16,3 +16,17 @@ File.exists?('./secret.text')
 
 # カレントディレクトリに"secret_folder"が存在するか？
 Dir.exists?('./secret_folder')
+
+
+# 以下、ファイルを読み書きする簡単なコード例
+
+# libディレクトリにあるfizz_buzz.rbの行数をターミナルに表示
+File.open('./lib/fizz_buzz.rb', 'r') do |f|
+  puts f.readlines.count
+end
+#=> 19
+
+# libディレクトリにhello_world.txtを作成して文字を書き込む
+File.open('./lib/hello_world.txt', 'w') do |f|
+  f.puts 'Hello, world!'
+end
