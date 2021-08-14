b85773c9e65ce8511ed267248d2cc18c5c2ed584
Ryo-exid <sakura4oneok@gmail.com>
2021-08-14 06:57:45 +0000

    Dateクラスで日付を表すオブジェクトを作成

--- else_topic/date_time.rb
+++ else_topic/date_time.rb
@@ -7,3 +7,7 @@ p time #=> 2017-01-31 23:30:50 +0000
 
 # dateライブラリをrequireするとDateクラスとDateTimeクラスが使えるようになる
 require 'date'
+
+# Dateクラスで日付を表すオブジェクトを作成
+date = Date.new(2017, 1, 31)
+p date #=> #<Date: 2017-01-31 ((2457785j,0s,0n),+0s,2299161j)>
