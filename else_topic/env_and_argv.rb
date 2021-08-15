30ea98b8c27173cb054a82556ddcca0984696719
Ryo-exid <sakura4oneok@gmail.com>
2021-08-15 15:09:14 +0000

    起動時引数の取得

--- else_topic/env_and_argv.rb
+++ else_topic/env_and_argv.rb
@@ -5,3 +5,16 @@ email = ARGV[0]
 age = ARGV[1]
 
 puts "name: #{name}, email: #{email}, age: #{age}"
+
+
+=begin
+
+
+# ターミナルで以下を入力
+
+$ export MY_NAME=ALICE
+
+$ ruby else_topic/env_and_argv.rb alice@example.com 20
+
+
+=end
